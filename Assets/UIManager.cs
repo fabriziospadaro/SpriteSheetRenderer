cb473a7d186ede3e6b5f8c094d54c1446d530826
Fabrizio Spadaro <f.spadaro@thefool.it>
2020-02-19 11:11:27 +0100

    Upgraded to unity 2019.3.1

---
+++ Assets/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class UIManager : MonoBehaviour {
+  public Image[] lifeImages;
+  public int lifeCount;
+  private int score;
+  public TextMeshProUGUI scoreText;
+  private static UIManager instance;
+  private void Start() {
+    instance = this;
+    AddScore(0);
+  }
+  public static void RemoveLife() {
+    instance.lifeImages[instance.lifeCount - 1].enabled = false;
+    instance.lifeCount--;
+  }
+  public static void AddScore(int value) {
+    instance.score += value;
+    instance.scoreText.text = "Score: " + instance.score.ToString();
+  }
+}
