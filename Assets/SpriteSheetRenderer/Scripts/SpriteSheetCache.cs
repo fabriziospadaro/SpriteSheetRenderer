4dcf3c73b6e4f848559064cbaac4834c4717e0f7
RenChen <renchenpro@gmail.com>
2020-06-18 15:09:19 +0800

    Fix single sprite spritesheet baking

--- Assets/SpriteSheetRenderer/Scripts/SpriteSheetCache.cs
+++ Assets/SpriteSheetRenderer/Scripts/SpriteSheetCache.cs
@@ -12,7 +12,20 @@ public static class SpriteSheetCache {
     Texture texture = sprites[0].texture;
     material.mainTexture = texture;
     if(sprites.Length == 1) {
-      return new KeyValuePair<Material, float4[]>(material, new float4[1] { new float4(1, 1, 0, 0) });
+      float w = texture.width;
+      float h = texture.height;
+      float4[] uvs = new float4[sprites.Length];
+      float tilingX = 1f / (w / sprites[0].rect.width);
+      float tilingY = 1f / (h / sprites[0].rect.height);
+      float OffsetX = tilingX * (sprites[0].rect.x / sprites[0].rect.width);
+      float OffsetY = tilingY * (sprites[0].rect.y / sprites[0].rect.height);
+      uvs[0].x = tilingX;
+      uvs[0].y = tilingY;
+      uvs[0].z = OffsetX;
+      uvs[0].w = OffsetY;
+      materialNameMaterial.Add(materialName, new KeyValuePair<Material, int>(material, sprites.Length));
+      materialToName.Add(material, materialName);
+      return new KeyValuePair<Material, float4[]>(material, uvs);
     }
     else {
       float w = texture.width;
